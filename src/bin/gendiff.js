c4aa3461fd444a0c9962ab399266cdeea0e32b6a
Tadamory <fortross59@gmail.com>
2019-12-27 00:08:24 +0600

    create format, option, action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,6 +3,12 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    firstConfigValue = firstConfig;
+    secondConfigValue = secondConfig;
+  });
 
 program.parse(process.argv);
