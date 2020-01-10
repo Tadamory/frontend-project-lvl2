b9f69f94fa96bb7e66ae089e02c117cf88bdc030
Ivan <fortross59@gmail.com>
2020-01-10 10:19:06 +0500

    update tests

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -1,11 +1,15 @@
 import compareFlatFiles from '../src/compareFlatFiles';
 
+//  const path = require('path');
+
 const targetStr = 'host: hexlet.io\n'
   + '- timeout: 50\n'
   + '+ timeout: 20\n'
   + '- proxy: 123.234.53.22\n'
   + '- follow: false\n'
   + '- verbose: true';
+
+//  далее нужно будет добавить тест на path.dirname для работы с абсолютными путями
 
 test('compareFlatFiles: relative path', () => {
   const firstPathToFile = '__test__/__fixtures__/before.json';
